975136937d4bf06c9184c106aa29b5d04ddd14d9
songmho <songmho@gmail.com>
2017-07-06 16:39:26 +0900

    0. first Commit

---
+++ AfterNoodle/AfterNoodle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AfterNoodle
+//
+//  Created by songmho on 2017. 7. 6..
+//  Copyright © 2017년 AfterNoodle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
