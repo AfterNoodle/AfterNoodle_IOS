408a4aa8c6f3324920ee451337216ca32d663c91
songmho <songmho@gmail.com>
2017-07-10 23:53:42 +0900

    0. tab bar 추

--- AfterNoodle/AfterNoodle/ViewController.swift
+++ AfterNoodle/AfterNoodle/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewWillDisappear(_ animated: Bool) {
+        var navigationBar = UINavigationBar.appearance()
+        navigationBar.tintColor = UIColor(white : 0xDB4C3F, alpha: 1.0)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
